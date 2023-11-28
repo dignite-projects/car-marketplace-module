f2df3f131f57a4ae7a639adbe1f99a65d5d8d2d5
杜冠魁 <hello@dignite.com>
2023-11-28 22:00:33 +0900

    Create SaleCarController.cs

---
+++ src/Dignite.CarMarketplace.HttpApi/Public/Cars/SaleCarController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Volo.Abp;
+
+namespace Dignite.CarMarketplace.Public.Cars
+{
+    [Area(CarMarketplaceRemoteServiceConsts.ModuleName)]
+    [RemoteService(Name = CarMarketplaceRemoteServiceConsts.RemoteServiceName)]
+    [Route("api/car-marketplace-public/sale-car")]
+    public class SaleCarController : CarMarketplaceController, ISaleCarAppService
+    {
+        private readonly ISaleCarAppService _saleCarAppService;
+
+        public SaleCarController(ISaleCarAppService saleCarAppService)
+        {
+            _saleCarAppService = saleCarAppService;
+        }
+
+        [HttpPost]
+        public async Task<SaleCarDto> CreateAsync(SaleCarCreateDto input)
+        {
+            return await _saleCarAppService.CreateAsync(input);
+        }
+    }
+}
