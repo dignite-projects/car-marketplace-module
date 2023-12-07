38d86dd1020d8aa76496717e04a8c3b369c749be
2416426467 <2416426467@qq.com>
2023-12-07 16:30:39 +0800

    提交修改

--- angular/projects/dev-app/src/app/home/home.component.ts
+++ angular/projects/dev-app/src/app/home/home.component.ts
@@ -1,7 +1,20 @@
+import { AuthService } from '@abp/ng.core';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  get hasLoggedIn(): boolean {
+    return this.authService.isAuthenticated;
+  }
+
+  constructor(private authService: AuthService) {
+  }
+
+
+  login() {
+    this.authService.navigateToLogin();
+  }
+}
