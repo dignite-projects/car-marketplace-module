a4e6a95349f21c344a434219daf0412144cf54c3
杜冠魁 <hello@dignite.com>
2023-12-18 09:06:31 +0900

    格式化价格和里程的文字

---
+++ src/Dignite.CarMarketplace.Web/Pages/CarMarketplacePageHelper.cs
@@ -0,0 +1,30 @@
+﻿using Volo.Abp.DependencyInjection;
+
+namespace Dignite.CarMarketplace.Web.Pages
+{
+    public class CarMarketplacePageHelper: ITransientDependency
+    {
+        public string GetPriceFormatText(float price)
+        {
+
+            switch (price)
+            {
+                case >= 10000:
+                    return $"{price/10000}万元";
+                default:
+                    return $"{price}元";
+            }
+        }
+        public string GetMileageFormatText(float totalMileage)
+        {
+
+            switch (totalMileage)
+            {
+                case >= 10000:
+                    return $"{totalMileage / 10000}万公里";
+                default:
+                    return $"{totalMileage}公里";
+            }
+        }
+    }
+}
