6b681beff1eac4980753d401fd2e3de464d86369
2416426467 <2416426467@qq.com>
2023-12-21 16:24:55 +0800

    优化文件引入

--- angular/projects/admin-lib/src/lib/admin-lib-routing.module.ts
+++ angular/projects/admin-lib/src/lib/admin-lib-routing.module.ts
@@ -2,28 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ManageComponent, SaleCarComponent, SaleCarViewComponent, UsedCarDetailsComponent, UsedCarListComponent } from './components';
-import { DynamicLayoutComponent, PermissionGuard, AuthGuard } from '@abp/ng.core';
 const routes: Routes = [
     {
-        path: 'manage',
-        component: ManageComponent,
+        path: '',
+        children: [{
+            path: 'manage',
+            component: ManageComponent,
+        },]
     },
     {
         path: 'sale-car',
-        children:[{
+        children: [{
             path: '',
             component: SaleCarComponent,
-        },{
+        }, {
             path: 'details/:id',
             component: SaleCarViewComponent,
         }]
     },
     {
         path: 'used-car',
-        children:[{
+        children: [{
             path: '',
             component: UsedCarListComponent,
-        },{
+        }, {
             path: 'details/:id',
             component: UsedCarDetailsComponent,
         }]
---
+++ angular/projects/admin-lib/src/lib/components/sale-car/index.ts
@@ -0,0 +1,2 @@
+export * from './sale-car-view.component';
+export * from './sale-car.component';
